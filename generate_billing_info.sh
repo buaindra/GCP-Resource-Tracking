40cb148ef6f284ec0b3bbef22605999efd6a5d7d
buaindra <indranil.bua@gmail.com>
2021-11-12 00:03:13 +0530

    added the file

--- generate_billing_info.sh
+++ generate_billing_info.sh
@@ -24,7 +24,22 @@ else
   echo "dataset is being created"
   bq --location=$LOCATION mk --dataset $PROJECT_ID:$DATASET
 fi
+
+
 #bq rm -r -f $DEVSHELL_PROJECT_ID:GCP_Billing
 #bq mk --table --expiration 36000 $DEVSHELL_PROJECT_ID:GCP_Billing.test_table
-SELECT * FROM `poc01-330806.GCP_Billing.gcp_billing_export_resource_v1_01F748_D68B6C_7BFEF3` WHERE DATE(_PARTITIONTIME) = "2021-11-11"
+#SELECT * FROM `poc01-330806.GCP_Billing.gcp_billing_export_resource_v1_01F748_D68B6C_7BFEF3` WHERE DATE(_PARTITIONTIME) = "2021-11-11"
+_billingtable=""
+for bq_t in $(gcloud alpha bq tables list  --dataset $DATASET --project $PROJECT_ID --format="csv[no-heading, separator=';'](id,tableReference.tableId)")
+do
+  IFS=';' read -r -a bqArray<<< "$bq_t"
+  echo "bqArray[0]: $bqArray[0]"
+  echo "bqArray[1]: $bqArray[1]"
+  echo "bqArray[2]: $bqArray[2]"
+	if [[ "${bqArray[1]}" == *"gcp_billing_export_resource_v1"* ]]; then _billingtable="${bqArray[1]}"; fi
+	if [[ -z "$_billingtable" ]]; then echo "Billing table created: $_billingtable"; else echo "Billing table not created"; fi
+	echo "${bqArray[1]: ${bqArray[0]}}"
+done
+
+
 
